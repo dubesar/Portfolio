603c628176d47bcb30c5175f25ce997f2148899c
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-05 22:53:50 +0530

    added onclick article

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,13 +1,32 @@
 import Image from "next/image"
+import { useState } from "react"
 
 export default function ArticleCard(article) {
+
+    const [cardStyle, setCardStyle] = useState('articleTitle');
+
+    const onHover = () => {
+        setCardStyle('articleTitleHover');
+    }
+
+    const onOutHover = () => {
+        setCardStyle('articleTitle');
+    }
+
+    const onClickLink = () => {
+        window.open(
+            `https://hashnode.com/${article.slug}`,
+            '_blank' // <- This is what makes it open in a new window.
+        );
+    }
+
     return (
-        <div>
+        <div onMouseEnter={onHover} onMouseLeave={onOutHover} onClick={onClickLink}>
             <div className="articleCard">
                 <Image src={article.cover} height="350" width="500" alt={article.title} />
             </div>
             <div>
-                <p className="articleTitle">{article.title}</p>
+                <p className={cardStyle}>{article.title}</p>
                 <p className="articleBrief">{article.brief}</p>
             </div>
         </div>   
