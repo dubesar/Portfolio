e97c59810d352bcea3cda05caa6bb2d218f46b16
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-05 14:25:01 +0530

    added

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -2,10 +2,16 @@ import Image from "next/image"
 
 export default function ArticleCard(article) {
     return (
-        <div className="articleCard">
-            <Image src={article.cover} height="350" width="500" alt={article.title} />
-            <p className="articleTitle">{article.title}</p>
-            <p className="articleBrief">{article.brief}</p>
-        </div>
+        <div>
+            <div className="articleCard">
+                <Image src={article.cover} height="350" width="500" alt={article.title} />
+            </div>
+            <div>
+                <div>
+                    <p className="articleTitle">{article.title}</p>
+                    <p className="articleBrief">{article.brief}</p>
+                </div>
+            </div>
+        </div>   
     )
 }
