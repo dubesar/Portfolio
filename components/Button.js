9e7cfdd4099da3a222ae5b75c6155edcb3ed1c42
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-05 18:59:46 +0530

    added links

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,14 @@
-export default function Button({text}) {
+import Image from "next/image"
+
+export default function Button({text, icon, link}) {
     return (
-        <div className="buttonComp">
+        <div onClick={() => {
+            window.location.href = link
+        }} className="buttonComp">
             {text}
+            {
+                icon ? <Image src={icon} alt="icon" width="20" height="20" /> : null
+            }
         </div>
     )
 }
--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,8 +6,8 @@ export default function HeroSection() {
             <div className="herotitle">Howdy! I’m Sarvesh Dubey...</div>
             <p className="heroinfo">I am a <span className="spacing">Full Stack Developer</span> and <span className="spacing">an Avid Reader</span> with a knack for building things. Currently building an Integrations stack at LambdaTest. My Educational Background includes B.Tech from NIT Surat.</p>
             <div className="buttonSection">
-                <Button text={'Know More'} />
-                <Button text={'Find me on Twitter'} />
+                <Button text={'Know More'} link="https://google.com"/>
+                <Button text={'Find me on Twitter'} icon="https://img.icons8.com/color/144/000000/twitter--v1.png" link={'https://twitter.com/Sarvesh12601156'} />
             </div>
         </div>
     )
