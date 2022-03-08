fb12df2ad6e17757828665e225e66be22cdb6c7b
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-09 00:35:08 +0530

    added posthog

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import '../styles/navbar.css'
 import '../styles/herosection.css'
 import '../styles/stacksection.css'
 import '../styles/article.css'
+import { usePostHog } from 'next-use-posthog'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
+  usePostHog('phc_rsJ6Frq8PKdP1csXClh7mhKGIUCmvouo5bIPlTm2NZo', { api_host: 'https://app.posthog.com' })
   return (
     <>
       <Navbar />
