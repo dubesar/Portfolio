9e2b7c3d15c0d411684dc14d32018c0243c4e19c
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-10 02:23:11 +0530

    added mail

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -15,6 +15,9 @@ export default function HeroSection() {
                 <Image onClick={() => {
                     window.location.href = 'https://www.linkedin.com/in/sarvesh-dubey-1b1505143/'
                 }} src="https://s2.svgbox.net/social.svg?ic=linkedin" width="30" height="30" alt="linkedin"/>
+                <Image onClick={() => {
+                     window.location.href = "mailto:dubeysarvesh5525@gmail.com";
+                }} src="https://s2.svgbox.net/materialui.svg?ic=email" width="40" height="40" alt="email"/>
             </div>
         </div>
     )
