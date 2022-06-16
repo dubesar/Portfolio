b263636e9cb7bced705e1287e120260afa0aecb3
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-06-16 16:14:37 +0530

    Update HeroSection.js

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,7 +4,7 @@ export default function HeroSection() {
     return (
         <div className="hero">
             <div className="herotitle">Howdy! I’m Sarvesh Dubey...</div>
-            <p className="heroinfo">I am a <span className="spacing">Full Stack Developer</span> and <span className="spacing">an Avid Reader</span> with a knack for building things. Currently building an Integrations stack at LambdaTest. My Educational Background includes B.Tech from NIT Surat.</p>
+            <p className="heroinfo">I am a <span className="spacing">Full Stack Developer</span> and <span className="spacing">an Avid Reader</span> with a knack for building things. Currently working with AWS ElasticSearch Team. My Educational Background includes B.Tech from NIT Surat.</p>
             <div className="buttonSection">
                 <Image onClick={() => {
                     window.location.href = 'https://twitter.com/Sarvesh12601156'
